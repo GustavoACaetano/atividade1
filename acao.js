f2746b248e668d899833f3894202d6d2d6df6a82
Gustavo Caetano <91470894+GustavoACaetano@users.noreply.github.com>
2021-10-15 17:41:30 -0300

    Update acao.js

--- acao.js
+++ acao.js
@@ -1,6 +1,7 @@
 const nomesComidas = document.querySelectorAll('h2');
 const comidas = new Array();
 const lista = document.querySelector('ol');
+const retornarDivs = document.querySelectorAll('div');
 
 nomesComidas.forEach(nomesComida => {
     comidas.push(nomesComida.textContent)
@@ -10,4 +11,8 @@ comidas.forEach(comida => {
     const linha = document.createElement('li');
     linha.innerHTML = '<a href="#'+ comida +'">' + comida + '</a>'; //usei innerHTML porque o textContent fazia aparecer tudo escrito ao invés de inserir o código
     lista.append(linha);
+});
+
+retornarDivs.forEach(retornarDiv => {
+    retornarDiv.innerHTML = '<a href="#lista">Retornar</a>';
 });
